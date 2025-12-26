42ad24c125d4cddff78488d873c2e354d9fbd77d
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-12-26 23:26:47 +0330

    db.connection added

---
+++ services/blog/config/db.connection.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose"
+
+export const db = mongoose.connect("mongodb://localhost:27017/grpc-js")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("DB connection error:", err));
